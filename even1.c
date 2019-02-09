83b255003541c89d889440408a969121e9f03ba4
kamala311 <47448744+kamala311@users.noreply.github.com>
2019-02-09 21:08:17 +0530

    Update even1.c

--- even1.c
+++ even1.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-
-int main(void) {
+int main() {
     int a,b,i;
     scanf("%d%d",&a,&b);
+    a=a+1;
     for(i=a;i<=b;i++)
     {
-        if(i!=6)
-        {
         if(i%2==0)
         {
             printf("%d ",i);
         }
-        }
     }
-	// your code goes here
 	return 0;
 }
